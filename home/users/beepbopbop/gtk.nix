b956cee8dc9ff9b7e7199f502ad0507a04744ec3
xKirtle <rodm.martins@proton.me>
2025-12-07 21:57:59 +0000

    Enable cursor on xwayland apps

--- home/users/beepbopbop/gtk.nix
+++ home/users/beepbopbop/gtk.nix
@@ -19,4 +19,13 @@
       package = pkgs.bibata-cursors;
     };
   };
+
+  # Ensure cursor theme is applied to both Wayland and X11 (Xwayland) apps.
+  home.pointerCursor = {
+    name = "Bibata-Modern-Ice";
+    package = pkgs.bibata-cursors;
+    size = 24;
+    gtk.enable = true;
+    x11.enable = true;
+  };
 }
