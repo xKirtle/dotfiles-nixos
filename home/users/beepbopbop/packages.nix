f9465ae44df98e055d181aefa2f5cca46ba528fc
xKirtle <rodm.martins@proton.me>
2025-12-21 05:28:49 +0000

    Add file, fragments, and lsfg packages; reorder starship in home packages list

--- home/users/beepbopbop/packages.nix
+++ home/users/beepbopbop/packages.nix
@@ -13,7 +13,9 @@
     dotnetCorePackages.sdk_10_0-bin
     eza
     fastfetch
+    file
     foliate
+    fragments
     freelens-bin
     go
     gcc
@@ -21,16 +23,19 @@
     jetbrains.goland
     jetbrains.rider
     libnotify
+    lsfg-vk
+    lsfg-vk-ui
     nwg-look
     pavucontrol
     powershell
     proton-pass
     protonup-qt
     protonvpn-gui
+    qbittorrent # can't decide between this and fragments
     sassc
     solaar
-    starship
     spotify
+    starship
     teams-for-linux
     unzip
     vesktop
