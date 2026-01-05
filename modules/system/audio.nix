20f27675366da0d3714a2097f396edfb5068af64
xKirtle <rodm.martins@proton.me>
2026-01-05 11:01:52 +0000

    Add wireplumber configuration to restore audio profile for Focusrite Scarlett Solo

--- modules/system/audio.nix
+++ modules/system/audio.nix
@@ -9,5 +9,30 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
+    
+    wireplumber.enable = true;
+
+    # Couldn't make wireplumber select the right profile for my Focusrite Scarlett Solo, so I did the next best thing: force it to remember the profile I set manually.
+    wireplumber.extraConfig."10-enable-profile-restore" = {
+      "wireplumber.settings" = {
+        "device.restore-profile" = true;
+      };
+    };
+
+    # Does not work
+    # wireplumber.extraConfig."50-scarlett-solo-profile" = {
+    #   "monitor.alsa.rules" = [
+    #     {
+    #       matches = [
+    #         { "device.name" = "alsa_card.usb-Focusrite_Scarlett_Solo_4th_Gen_S14AD9G3B27480-00"; }
+    #       ];
+    #       actions = {
+    #         update-props = {
+    #           "device.profile" = "HiFi (Line1, Line2, Mic1, Mic2)";
+    #         };
+    #       };
+    #     }
+    #   ];
+    # };
   };
 }
