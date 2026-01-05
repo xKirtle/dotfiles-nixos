0f22c43c03869aa4f98e3b0305148019d9bba7e4
xKirtle <rodm.martins@proton.me>
2026-01-05 10:19:55 +0000

    Add Azure SSH key configuration and update allowed signers

--- home/users/beepbopbop/ssh.nix
+++ home/users/beepbopbop/ssh.nix
@@ -12,9 +12,17 @@
         identityFile = "~/.ssh/github_xkirtle_ed25519";
         identitiesOnly = true;
       };
+      "ssh.dev.azure.com" = {
+        user = "git";
+        hostname = "ssh.dev.azure.com";
+        identityFile = "~/.ssh/azure_unit4_rsa";
+        identitiesOnly = true;
+      };
     };
   };
 
   home.file.".ssh/github_xkirtle_ed25519".source = ./files/ssh/secrets/github_xkirtle_ed25519;
   home.file.".ssh/github_xkirtle_ed25519.pub".source = ./files/ssh/secrets/github_xkirtle_ed25519.pub;
+  home.file.".ssh/azure_unit4_rsa".source = ./files/ssh/secrets/azure_unit4_rsa;
+  home.file.".ssh/azure_unit4_rsa.pub".source = ./files/ssh/secrets/azure_unit4_rsa.pub;
 }
