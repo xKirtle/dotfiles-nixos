efe6c175e487e2bcef98c48d2435dd77dafc2d97
xKirtle <rodm.martins@proton.me>
2026-01-20 23:07:52 +0000

    Refactor boot.nix to include NixOS unstable kernel and adjust system configuration

--- home/users/beepbopbop/configFiles.nix
+++ home/users/beepbopbop/configFiles.nix
@@ -4,4 +4,5 @@
   home.file.".config/solaar/config.yaml".source = ./files/solaar/config.yaml;
   home.file.".config/teams-for-linux/config.json".source = ./files/teams-for-linux/config.json;
   home.file.".config/gtk-3.0/bookmarks".source = ./files/gtk-3.0/bookmarks;
+  home.file.".config/gtk-3.0/bookmarks".force = true;
 }
--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -1,6 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 
-{
+let
+  u = import <nixos-unstable> {
+    system = pkgs.stdenv.hostPlatform.system;
+    config.allowUnfree = true;
+  };
+in {
+  # Use the unstable kernel to match the NVIDIA driver from <nixos-unstable>. Comment out to fallback to the stable kernel.
+  boot.kernelPackages = u.linuxPackages;
+
   boot.loader = {
     systemd-boot = {
       enable = true;
